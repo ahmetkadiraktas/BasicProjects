1abc538a35ed17bda4a9a79a93d5575fc255d221
ahmetkadiraktas <ahmetkadiraktas7@gmail.com~>
2023-05-01 00:25:30 +0300

    first commit

---
+++ MyWebSite/deneme1.js
@@ -0,0 +1,18 @@
+function openNav() {
+    document.getElementById("myNav").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNav() {
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+  function openNavv() {
+    document.getElementById("myNavv").style.width = "100%";
+  }
+  
+  /* Close when someone clicks on the "x" symbol inside the overlay */
+  function closeNavv() {
+    document.getElementById("myNavv").style.width = "0%";
+  }
+
